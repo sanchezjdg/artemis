4d87e7bed454a87b869181cf3fd9fb2d0c7f2331
juanqui2907 <jfq123@gmail.com>
2025-03-26 17:06:25 -0500

    zoom

--- web-server/public/app.js
+++ web-server/public/app.js
@@ -77,7 +77,7 @@ socket.on('updateData', (data) => {
 
     realTimePath.setLatLngs(realTimeCoordinates.map(coord => [coord.latitude, coord.longitude]));
 
-    map.setView(latlng, 15, { animate: true });
+    map.setView(latlng, map.getZoom(), { animate: true });
 
     marker.bindPopup(`
       <strong>Current Position</strong><br>
