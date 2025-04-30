02d7c2e874c87425932ff89c94972d34982d0629
juanqui2907 <jfq123@gmail.com>
2025-04-30 09:53:13 -0500

    a

--- web-server/public/js/historicalMode.js
+++ web-server/public/js/historicalMode.js
@@ -200,14 +200,12 @@ loadButton.addEventListener('click', async () => {
 
     // Reactivar trace si ya estaba encendido antes de cargar
     const traceToggle = document.getElementById("enable-trace-toggle");
-    const map = getMap();
-    map.off("click", onMapClickTrace); // Evita duplicados
-    
     if (traceToggle.checked) {
+      getMap().off("click", onMapClickTrace); // Elimina handler duplicado si existía
       const event = new Event("change");
       traceToggle.dispatchEvent(event);
-    }
-    
+    }    
+
   } catch (err) {
     console.error('Error loading data:', err);
     showToast('Error loading route data.');
