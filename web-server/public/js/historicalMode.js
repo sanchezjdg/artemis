feae27617ea33833aa0e31feed3346da3b67fd4f
juanqui2907 <108438932+juanqui2907@users.noreply.github.com>
2025-05-21 14:54:41 -0500

    Jfelipe (#147)
    * a
    
    * a
    
    * trace bugs
    
    * trace marco
    
    * trace marco
    
    * trace marco
    
    * trace marco
    
    * trace marco
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * trace flecha
    
    * celular
    
    * celular
    
    * celular
    
    ---------
    
    Co-authored-by: juanqui2907 <jfq123@gmail.com>

--- web-server/public/js/historicalMode.js
+++ web-server/public/js/historicalMode.js
@@ -188,12 +188,13 @@ export function initHistoricalMode() {
       if (document.getElementById("trace-time-slider-control").style.display === "block") {
         document.getElementById("trace-time-slider-control").style.display = "block";
       }
-      toggleButton.innerHTML = "▲ Hide Options";
+      toggleButton.innerHTML = "▼ Show Options";
     } else {
       traceOptionsContainer.style.display = "none";
       document.getElementById("trace-radius-control").style.display = "none";
       document.getElementById("trace-time-slider-control").style.display = "none";
-      toggleButton.innerHTML = "▼ Show Options ";
+      toggleButton.innerHTML = "▲ Hide Options";
+
     }
   });
 
